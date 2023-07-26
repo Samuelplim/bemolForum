f747fd478fd32377b52af9ec111b5feb68e80eef
samuelplim <samuel12nerd@gmail.com>
2023-07-26 13:31:11 -0400

    [refactor] melhoria nos middle

---
+++ backend/src/middlewares/error.middlewares.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+const AppError = require("./utils/AppError");
+
+export const errorMiddleware = (
+  error: any,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+};
