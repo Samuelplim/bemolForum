65553b5a336005cd1f779d56eb96bd124b9a9d1b
samuelplim <samuel12nerd@gmail.com>
2023-07-27 22:57:20 -0400

    [add] footer

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -3,5 +3,6 @@ import { InputLarge } from "./InputLarge";
 import { LinkLight } from "./LinkLight";
 import { NavMenu } from "./NavMenu";
 import { VerticalList } from "./VerticalList";
+import { Footer } from "./Footer";
 
-export { ButtonLarge, InputLarge, LinkLight, NavMenu, VerticalList };
+export { ButtonLarge, InputLarge, LinkLight, NavMenu, VerticalList, Footer };
