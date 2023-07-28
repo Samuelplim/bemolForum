f7eccb844910492b4b10a656d0373dac17e36f93
samuelplim <samuel12nerd@gmail.com>
2023-07-27 21:41:29 -0400

    [add] component VerticalList

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -2,5 +2,6 @@ import { ButtonLarge } from "./ButtonLarge";
 import { InputLarge } from "./InputLarge";
 import { LinkLight } from "./LinkLight";
 import { NavMenu } from "./NavMenu";
+import { VerticalList } from "./VerticalList";
 
-export { ButtonLarge, InputLarge, LinkLight, NavMenu };
+export { ButtonLarge, InputLarge, LinkLight, NavMenu, VerticalList };
