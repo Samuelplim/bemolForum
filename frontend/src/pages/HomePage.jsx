c5ee01782090c8db4366c8b7d689946f6a76e74f
samuelplim <samuel12nerd@gmail.com>
2023-07-27 21:42:19 -0400

    [feat] getapi

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,18 @@
-import { useEffect } from "react";
-import { ButtonLarge, NavMenu } from "../components";
+import { useEffect, useState } from "react";
+import { ButtonLarge, NavMenu, VerticalList } from "../components";
 import { io } from "socket.io-client";
+import { indexTopics } from "../services/topics.services";
+
 
 
 export const HomePage = () => {
+
+  const {topics, setTopics} = useState([])
+
+  /*
   useEffect(() => {
     // Connect to the Socket.IO server
-    const socket = io('http://localhost:3000'); // Replace with your server URL
+    const socket = io(import.meta.env.VITE_BACKEND_URL); // Replace with your server URL
 
     // Listen for the "connect" event, indicating a successful connection
     socket.on('connect', () => {
@@ -24,18 +30,31 @@ export const HomePage = () => {
       socket.disconnect();
     };
   }, []);
+*/
 
+  useEffect(() => {
+/*
+    (async () =>{
+      const response = await indexTopics()
+      setTopics(response);
+    })()
+*/
+  },[])
 
   return (
     <>
     <header><NavMenu/></header>
-    <main className="bg-naturalWhite h-screen">
+    <main className="bg-naturalWhite">
      <div className=" mt-4 px-2">
       <p>Não encontrou o que procura?</p>
       <ButtonLarge title="criar um topico" className="" />
     </div>
 
-     
+    <section className=" bg-light-200 mt-4 h-96">
+     {topics ? topics.map(topic => (
+      <VerticalList key={String(topic._id)} data={topic} />
+     )):(<div className="h-full items-center flex px-4"> <p className="font-medium text-center">Não encontrei nenhum topico no momento, tente criar um novo topico ou volte mais tarde!</p></div>)}
+     </section>
     </main>
     </>
   );
