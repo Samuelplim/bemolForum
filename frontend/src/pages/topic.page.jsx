a14acd6a12adb78247941bf9d530bbfb07096cb5
samuelplim <samuel12nerd@gmail.com>
2023-07-29 17:03:23 -0400

    [feat] fix

--- frontend/src/pages/topic.page.jsx
+++ frontend/src/pages/topic.page.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react"
 import { useParams} from "react-router-dom"
-import { Footer, Header } from "../components"
+import { CommentsList, Footer, Header, InputLarge } from "../components"
 import { FindByIdTopics } from "../services/topics.services"
 
 export const TopicPage = ()=> {
+  const [data, setData] = useState(null);
+  const params = useParams()
+
+  useEffect(() => {
+    async function fetchNote(){
+      const result = await FindByIdTopics(params).then(respons=>{return respons})
+      if(result){
+        setData(result)
+      }
+    }
+    fetchNote()
+  }, [])
+  
   return(
     <>
     <Header/>
     <main className="bg-naturalWhite">
+      {data?
       <div className=" mt-4 px-2">
-        <p>Não encontrou o que procura?</p>
+        <p>{data.data.title}</p>
+        <p>{data.data.content}</p>
+        <CommentsList comments={data.data.comments} />
       </div>
+      :<p>Carregando</p>}
+      <InputLarge placeholder={"Menssagem"} paperAirPlane/>
     </main>
     <Footer />
     </>
