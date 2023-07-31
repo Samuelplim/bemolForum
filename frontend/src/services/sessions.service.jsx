2a2d39a9e6e4dadd5a48b7cabf815d42936d81bc
samuelplim <samuel12nerd@gmail.com>
2023-07-31 15:16:15 -0400

    [feat] set Auth Token in backend

--- frontend/src/services/sessions.service.jsx
+++ frontend/src/services/sessions.service.jsx
@@ -4,3 +4,13 @@ export async function createSessions(customer) {
   const response = await backendApi.post("/sessions",customer);
   return response;
 }
+
+export const setAuthToken = (token) => {
+  if (token) {
+    // Se um token válido for fornecido, inclua-o no cabeçalho de todas as requisições
+    backendApi.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    // Se o token for nulo ou inválido, remova-o do cabeçalho
+    delete backendApi.defaults.headers.common['Authorization'];
+  }
+};
