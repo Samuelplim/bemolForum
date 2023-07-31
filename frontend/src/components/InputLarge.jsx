8a741bd20fe65d7e8fff32e7fd59b0779697f423
samuelplim <samuel12nerd@gmail.com>
2023-07-31 19:10:34 -0400

    [refactor] title optional render

--- frontend/src/components/InputLarge.jsx
+++ frontend/src/components/InputLarge.jsx
@@ -3,10 +3,10 @@ export const InputLarge = ({title, placeholder, paperAirPlane, magnifyingGlass, 
 
   return  (
   <div className="w-full flex flex-col items-start gap-2">
-    <span className="text-dark-400">{title}</span>
+    {title && <span className="text-dark-400">{title}</span>}
     <div className="w-full flex bg-light-100  rounded-lg border-tints-cake-200 border-2">
       <input placeholder={placeholder} type="text" className="w-full px-3.5 py-3 rounded-lg" {...rest}/>
-      { paperAirPlane && <button className="mr-1"><PaperAirplaneIcon className="h-6 w-6 text-tints-cake-200"/></button>}
+      { paperAirPlane && <button className="mr-1"><PaperAirplaneIcon className="h-6 w-6 text-tints-cake-200 self-center"/></button>}
       
       {magnifyingGlass && <button className="mr-1">
         <MagnifyingGlassIcon className="h-6 w-6 text-tints-cake-200"/>
