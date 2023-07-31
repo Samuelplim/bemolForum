8b5bc6ce1ce7ee8f41cef2e77b36c3e2777f33ce
samuelplim <samuel12nerd@gmail.com>
2023-07-31 19:09:40 -0400

    [feat] styled comments

--- frontend/src/components/CommentsList.jsx
+++ frontend/src/components/CommentsList.jsx
@@ -1,17 +1,21 @@
+import { DateTime } from "luxon"
+
+import { useAuth } from "../hooks/auth"
 
 export const CommentsList = ({comments, ...rest }) => {
+  const { customer } = useAuth()
   const _renderItems = (item, index)=>{
-    console.log("", item);
+    const isAthor = item.author._id ===customer._id
     return (
-    <div key={index}>
-      <p>{item.author.name}</p>
+    <div key={index} className={ isAthor ?"flex flex-col bg-blue-50 rounded-md p-2 max-w-sm":"flex flex-col bg-blue-100 rounded-md p-2 max-w-sm self-end"}>
+      {isAthor && <p className=" font-medium text-xs">{item.author.name}</p>}
       <p>{item.content}</p>
-      <span>{item.createdAt}</span>
+      <p className="self-end text-xs">{DateTime.fromISO(item.createdAt).setLocale('pt').toFormat('MM:HH')}</p>
     </div>
     )}
 
   return (
-    <div {...rest} className="bg-blue-50">
+    <div {...rest} className="flex flex-col items-start gap-2 my-3">
       {comments ? comments.map(_renderItems):<p>Seja o primeiro a comentar!</p>}
     </div>
   );
