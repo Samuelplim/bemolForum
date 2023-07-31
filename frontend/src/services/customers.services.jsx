75191faddfe15064e2c5eb40f77fd2c8d0415264
samuelplim <samuel12nerd@gmail.com>
2023-07-30 21:42:01 -0400

    [feat] add functions findByEmail and del customer

--- frontend/src/services/customers.services.jsx
+++ frontend/src/services/customers.services.jsx
@@ -6,3 +6,13 @@ export async function createCustomers(customer) {
   const response = await backendApi.post("/customer",customer);
   return response;
 }
+
+export async function findByEmailCustomers(customer) {
+  const response = await backendApi.get("/customer",customer);
+  return response;
+}
+
+export async function deleteCustomers(customer) {
+  const response = await backendApi.delete("/customer",customer);
+  return response;
+}
