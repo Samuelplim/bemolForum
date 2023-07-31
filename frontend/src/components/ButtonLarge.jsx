fc956dd8dfc0068011ecf7fa9a216546ca9e5600
samuelplim <samuel12nerd@gmail.com>
2023-07-31 16:01:47 -0400

    add props children

--- frontend/src/components/ButtonLarge.jsx
+++ frontend/src/components/ButtonLarge.jsx
@@ -1,8 +1,8 @@
-export const ButtonLarge = ({title})=> {
+export const ButtonLarge = ({title, ...rest})=> {
 
   return  (
  
-    <button className="w-full px-3.5 py-3 rounded-md text-sm bg-tints-cake-200 hover:bg-tints-cake-150 disabled:bg-tints-cake-100 text-white">
+    <button {...rest} className="w-full px-3.5 py-3 rounded-md text-sm bg-tints-cake-200 hover:bg-tints-cake-150 disabled:bg-tints-cake-100 text-white">
     {title}
     </button>
  
--- frontend/src/components/InputLarge.jsx
+++ frontend/src/components/InputLarge.jsx
@@ -2,10 +2,10 @@ import { MagnifyingGlassIcon, PaperAirplaneIcon } from '@heroicons/react/24/soli
 export const InputLarge = ({title, placeholder, paperAirPlane, magnifyingGlass, ...rest})=> {
 
   return  (
-  <div className="w-full flex flex-col items-start gap-2" {...rest}>
+  <div className="w-full flex flex-col items-start gap-2">
     <span className="text-dark-400">{title}</span>
     <div className="w-full flex bg-light-100  rounded-lg border-tints-cake-200 border-2">
-      <input placeholder={placeholder} type="text" className="w-full px-3.5 py-3 rounded-lg"/>
+      <input placeholder={placeholder} type="text" className="w-full px-3.5 py-3 rounded-lg" {...rest}/>
       { paperAirPlane && <button className="mr-1"><PaperAirplaneIcon className="h-6 w-6 text-tints-cake-200"/></button>}
       
       {magnifyingGlass && <button className="mr-1">
